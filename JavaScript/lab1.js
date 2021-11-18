30d5da005a279a96455989a3b9c12cb827ad8e01
aligulfaamstudent <Ali_Gulfaam@student.mahoningCTC.com>
2021-11-18 12:40:26 -0500

    first commit

---
+++ JavaScript/lab1.js
@@ -0,0 +1,19 @@
+// Javascript document
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAnHour = secondsInAMinute * minutesInAHour;
+document.write("the Number of seconds in an hour is " + secondsInAnHour);
+document.write("<br>")
+
+var secondsInADay = secondsInAMinute * minutesInAHour;
+document.write("The number of seconds in a day are " + secondsInADay);
+document.write("<br>");
+
+var secondsInAYear = secondsInADay * daysInAYear;
+document.write("The number of seconds in a year are " + secondsInAYear);
+document.write("<br>");
+document.write("<br>");
+document.write("Created by Gulfaam");
---
+++ JavaScript/lab1_2.js
@@ -0,0 +1,24 @@
+// Javascript Document
+var numberofApples = 10;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+document.write("the number of apples i have is " + numberofApples + "<br>");
+numberofApples++;
+
+document.write("<br><br>");
+
+var numberOfNinja = 100;
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>");
+document.write("Created by Gulfaam")
